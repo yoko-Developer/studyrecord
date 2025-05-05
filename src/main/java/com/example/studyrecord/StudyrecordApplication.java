05fa5a9031670cf1de2fa9967c6f69dd12a934d8
yoko-Developer <whitesnow.wave@gmail.com>
2025-05-05 18:43:07 +0900

    initial commit

---
+++ src/main/java/com/example/studyrecord/StudyrecordApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyrecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyrecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studyrecord/StudyrecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyrecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
